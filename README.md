20934379238b4778a66860d9b6149babd5e49fe2
Nathalia <110792289+ndamasc@users.noreply.github.com>
2025-05-02 14:51:43 -0400

    acessando paginas de cadastro e home

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Cypress-project
 Creating a simple project to learn more about Cypress
 
+### Inicializando a ferramenta
 
-npm init -y
+'npm init -y'
 
-npx cypress open
+'npx cypress open'
+
+
+### Roteiro de Testes
+
+1. Realizar cadastro no site
+
+- Acessar pagina de cadastro
+- Preencher campos de "nome"
+- Preencher campos de "email" com endereço valido
+- Preencher campos de "senha" com uma senha valida
+- Preencher campos de "Confirmação de senha" 
+- Clicar no botao cadastrar
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,29 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/')
+    cy.contains('a', 'Cadastrar').click();
+    cy.get('input[name="nome"]').type('Amanda Bynes');
+    cy.get('input[name="email"]').type('amanda123@gmail.com');
+    cy.get('input[name="password"]').type('Senha123');
+    cy.get('input[name="confirm_password"]').type('Senha123');
+    cy.contains('button', 'Cadastrar').click();
+  })
+})
+
+
+describe('teste de login', () => {
+  it('passes', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/')
+    cy.contains('a', 'Fazer login').click();
+    cy.get('input[name="email"').type('amanda123@gmail.com');
+    cy.get('input[name="password"').type('Senha123');
+    cy.contains('button', 'Entrar').click();
+  })
+})
+
+describe('entrando na pagina home', () => {
+  it('passes', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/home')
+
   })
 })
